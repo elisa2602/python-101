c260d0fc334dc4317db7e59536395ed71153fa6c
Elisa <93845510+elisa2602@users.noreply.github.com>
2022-11-26 17:39:11 +0100

    Update pattern.py

--- 1-hello-world/pattern.py
+++ 1-hello-world/pattern.py
@@ -1,4 +1,8 @@
-print('       1')
-print('     2 3')
-print('   4 5 6')
-print('7 8 9 10')
+r = 4 #nb of rows
+n = 1
+
+for i in range(1,r+1):
+    for j in range(1,i+1):
+        print(n, end =' ')
+        n += 1  
+    print()
